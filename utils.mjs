7a7dbc1cc76df1f6040bb35995df2bce5cfb3949
Vishal Jadhav <143590501+vishaljadhav7@users.noreply.github.com>
2024-06-28 12:06:07 +0530

    utility functions

--- utils.mjs
+++ utils.mjs
@@ -1,46 +1,25 @@
-import {getSuggestions, debounce} from "./utils.mjs";
+import { FRUITS } from "./data.mjs";
 
-const inputBox = document.querySelector('#search-input');
-const suggestionsContainer = document.querySelector('#suggestions-wrapper');
-
-function resetState(){
-    suggestionsContainer.classList.remove('suggestions-visible')
-    suggestionsContainer.innerHTML = ''
+export const getSuggestions =  (query) =>{
+   const result = FRUITS.filter(item => {
+    return  (item.substring(0, query.length).toLocaleLowerCase() === query.toLocaleLowerCase())
+   }) 
+   
+   return new Promise((res)=>{
+       setTimeout(()=>{
+           res(result)
+       },500)
+   })
 }
 
-const addSuggestions = (list = []) =>{
-    const fragment = document.createDocumentFragment()
-    list.forEach((item)=>{
-       const div = document.createElement('div');
-       div.innerHTML = item;
-       div.classList.add('list-item');
-       fragment.appendChild(div) 
-    })
-    suggestionsContainer.innerHTML = '' 
-    suggestionsContainer.appendChild(fragment)
+export function debounce(fn, delay = 500){
+    let id;
+    return function(){
+        let self = this, args = arguments;
+        clearTimeout(id);
+        id = setTimeout(()=>{
+           fn.apply(self, args)
+        },delay)
+    }
 }
 
-const handleSearch = async (query) =>{
-  const suggestions =  await getSuggestions(query);
-  console.log(`for query ${query}`, suggestions)
-  if(suggestions.length){
-    suggestionsContainer.classList.add('suggestions-visible')
-    addSuggestions(suggestions);
-  }
-  
-}
-
-const handleInputChange = (e) =>{
- const {value} = e.target;
- if(value){
-    handleSearch(value);
- }else{
-    resetState()
-  }
-}
-
-
-(()=>{
-    inputBox.addEventListener('input',debounce(handleInputChange, 1500));
- }
-)();
